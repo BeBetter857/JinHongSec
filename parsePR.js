2feaad75af6debeaf7d997240902e139c5c142ca
赵金红 <irving.7758520@gmail.com>
2024-04-17 23:15:37 +0800

    PRTestClassParse

---
+++ parsePR.js
@@ -0,0 +1,33 @@
+/*
+ * @Author: kyrie zhao
+ * @Date: 2024-04-17 23:15:15
+ * @LastEditors: kyrie zhao
+ * @LastEditTime: 2024-04-17 23:15:16
+ * @Description: 
+ */
+const fs = require('fs');
+const readline = require('readline')
+
+async function extractTests(){
+
+    //by default we specify that all tests should run
+    let testsFile = __dirname+'/testsToRun.txt';
+    await fs.promises.writeFile(testsFile,'all');
+
+    const lines = readline.createInterface({
+        input: fs.createReadStream(__dirname+'/pr_body.txt'),
+        crlfDelay: Infinity
+    });
+
+    for await (const line of lines) {
+        //special delimeter for apex tests
+        if(line.includes('Apex::[') && line.includes(']::Apex')){
+
+            let tests = line.substring(8,line.length-7);
+            await fs.promises.writeFile(testsFile,tests);
+            await fs.promises.appendFile(testsFile,'\n');
+        }
+    }
+}
+
+extractTests();
